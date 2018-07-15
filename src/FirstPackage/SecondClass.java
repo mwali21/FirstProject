ab32d4e28e16048d280f8e6af62b1e6317cbe158
nmmani37 <nmmani37@gmail.com>
2018-07-15 11:49:03 +0530

    Added fifth class and edited second class

--- src/FirstPackage/SecondClass.java
+++ src/FirstPackage/SecondClass.java
@@ -1,6 +1,9 @@
 package FirstPackage;
 
 public class SecondClass {
+    public void m1(){
+        System.out.println("m1");
+    }
     public static void main(String[] args) {
         System.out.println("Im in second class");
     }
