dd25085326cad0f7d5ddeb1a9fddacb0fc8a6869
nmmani37 <nmmani37@gmail.com>
2018-07-15 11:36:31 +0530

    Added fourth and second class.

---
+++ src/FirstPackage/FourthClass.java
@@ -0,0 +1,7 @@
+package FirstPackage;
+
+public class FourthClass {
+    public static void main(String[] args) {
+        System.out.println("Fourth class");
+    }
+}
---
+++ src/FirstPackage/SecondClass.java
@@ -0,0 +1,7 @@
+package FirstPackage;
+
+public class SecondClass {
+    public static void main(String[] args) {
+        System.out.println("Im in second class");
+    }
+}
