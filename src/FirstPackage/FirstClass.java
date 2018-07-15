d2f96e963026bba925911bf5ad40ba6bedddefe9
nmmani37 <nmmani37@gmail.com>
2018-07-15 12:59:01 +0530

    Edited first class in the branch

--- src/FirstPackage/FirstClass.java
+++ src/FirstPackage/FirstClass.java
@@ -2,9 +2,11 @@ package FirstPackage;
 
 public class FirstClass {
     int num1 = 5, num2 = 15, sum;
-    sum = num1 + num2;
+    public void method(){
+        sum = num1 + num2;
+        System.out.println("Sum of these numbers: "+sum);
+    }
 
-      System.out.println("Sum of these numbers: "+sum);
 
 
 }
